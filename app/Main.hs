6868a76564f1cabb8955baff8fd34a2f8c14b2c6
Erik Rantapaa <erantapaa@gmail.com>
2015-07-24 11:06:00 -0500

    Got a simple app working.

--- app/Main.hs
+++ app/Main.hs
@@ -16,6 +16,16 @@ import qualified Data.ByteString.Lazy       as LB
 import qualified Data.ByteString.Lazy.Char8 as LC
 import           Data.Monoid
 
+import           System.Environment
+import TarUtil
+import Pipes
+import Text.Printf
+import qualified Data.Text.Lazy.Encoding  as LE
+import qualified Data.Text.Lazy as LT
+import System.Exit
+import Data.Time.Clock
+import Control.Monad
+
 -- ------------------------------------------------------------
 
 outputValue :: (ToJSON c) => String -> c -> IO ()
@@ -44,16 +54,31 @@ jsonOutput pretty io x
 jsonPath :: String -> String
 jsonPath fn = "json" </> fn <.> "js"
 
+processEntry now i = do
+  pe <- await
+  case LE.decodeUtf8' (pe_content pe) of
+    Left e    -> lift $ putStrLn $ "utf8 decode error on " ++ pe_cabalname pe
+    Right txt -> case parseCabal (LT.unpack txt) of
+                   Nothing -> do lift $ putStrLn $ "error parse cabal for " ++ pe_cabalname pe
+                                 processEntry now i
+                   Just pkgInfo -> do let pkgName = p_name pkgInfo
+                                          cmd = toCommand False now True [(pkgName, Just pkgInfo)]
+                                          path = printf "out-%05d" (i::Int)
+                                      lift $ outputValue path cmd
+                                      lift $ putStrLn $ "processed " ++ pkgName
+                                      processEntry now (i+1)
+
 main :: IO ()
 main = do 
-  cabal <- getContents
-  case parseCabal cabal of
-    Nothing -> error "oops"
-    Just pkgInfo -> do
-      now <- getCurrentTime
-      let pkgName = p_name pkgInfo
-      let cmd = toCommand True now True [(pkgName, Just pkgInfo)]
-      outputValue "output.json" cmd
-      return ()
-      -- toCommand :: Bool -> UTCTime -> Bool -> [(String, Maybe PackageInfo)] -> Command 
-
+  args <- getArgs
+  when (null args) $ do
+    putStrLn $ "Usage: app index.tar.gz"
+    exitFailure
+  let path = head args
+  entries <- tarEntriesForPath path
+  now <- getCurrentTime
+  runEffect $ pipesTarEntries entries >-> pipesSelectCabals >-> pipesLatestVersions >-> processEntry now 1 -- pipesShowEntries
+  end <- getCurrentTime
+  let diff = diffUTCTime end now
+  putStrLn $ "time: " ++ show diff
+  exitSuccess

e50d0b6530c46e1e0e7ca7d19dd9a93e40177f97
Erik Rantapaa <erantapaa@gmail.com>
2015-08-10 01:46:50 -0500

    Rework of PkgIndexerCore2 and JsonUtil

--- src/JsonUtil.hs
+++ src/JsonUtil.hs
@@ -5,16 +5,31 @@ module JsonUtil
   , jsonPutStr
   , hJsonPutStr
   , outputValue
+  , UTCTime
+  , fmtDateXmlSchema
+  , fmtDateHTTP
+  , pair
+  , APair
+  , A.toJSON
+  , A.Value(..)
+  , A.object
   )
 where
 
 import           Control.Exception          (bracket)
 import           Data.Aeson                 (ToJSON, encode)
+import qualified Data.Aeson                 as A (ToJSON, toJSON, object, Value(..))
 import           Data.Aeson.Encode.Pretty   (Config(..), encodePretty', keyOrder)
 import qualified Data.ByteString.Lazy       as LB
 import qualified Data.ByteString.Lazy.Char8 as LC
 import           Data.Monoid                (Monoid(..))
 import           System.IO
+
+import qualified Data.Text                  as T
+import           Data.Text                  (Text)
+import           Data.Time                  (UTCTime)
+import           Data.Time.Format           (formatTime)
+import           System.Locale              (defaultTimeLocale)
 
 jsonOutput :: (ToJSON c) => Bool -> (LB.ByteString -> IO a) -> c -> IO a
 jsonOutput pretty io x
@@ -36,4 +51,24 @@ hJsonPutStr pretty h c = jsonOutput pretty (LC.hPutStrLn h) c
 
 outputValue :: ToJSON c => FilePath -> c -> IO ()
 outputValue path c = bracket (openBinaryFile path WriteMode) hClose (\h -> hJsonPutStr True h c)
+
+--- Command building utilities
+
+fmtDateXmlSchema :: UTCTime -> String
+fmtDateXmlSchema = fmtDate' "%FT%X"
+
+fmtDateHTTP :: UTCTime -> String
+fmtDateHTTP = fmtDate' "%a %b %e %H:%M:%S %Z %Y"
+
+fmtDate' :: String -> UTCTime -> String
+fmtDate' fmt
+    = formatTime defaultTimeLocale fmt
+
+pair :: A.ToJSON c => String -> c -> (Text, A.Value)
+pair k v = (T.pack k, A.toJSON v)
+
+type APair = (Text, A.Value)
+
+buildNOOP :: A.Value
+buildNOOP = A.object [ pair "cmd" ("noop" :: String) ]
 
--- src/PkgIndexerCore2.hs
+++ src/PkgIndexerCore2.hs
@@ -1,38 +1,15 @@
 module PkgIndexerCore2
 where
 
-import qualified Data.Aeson                   as A
-import qualified Data.Vector                  as V
-import qualified Data.Text                    as T
-import           Data.Text                    (Text)
-
-import           PackageInfo                  (PackageInfo(..))
-
-import           Data.Time                    (UTCTime)
-import           Data.Time.Format             (formatTime)
-import           System.Locale                (defaultTimeLocale)
-import           Data.Scientific              as S
-import           Data.List                    (intercalate)
-
-fmtDateXmlSchema :: UTCTime -> String
-fmtDateXmlSchema = fmtDate' "%FT%X"
-
-fmtDateHTTP :: UTCTime -> String
-fmtDateHTTP = fmtDate' "%a %b %e %H:%M:%S %Z %Y"
-
-fmtDate' :: String -> UTCTime -> String
-fmtDate' fmt
-    = formatTime defaultTimeLocale fmt
-
-pair :: A.ToJSON c => String -> c -> (Text, A.Value)
-pair k v = (T.pack k, A.toJSON v)
-
-type APair = (Text, A.Value)
+import           PackageInfo     (PackageInfo(..))
+import           Data.List       (intercalate)
+import           JsonUtil        (UTCTime, fmtDateXmlSchema, fmtDateHTTP
+                                 ,pair, APair, Value, toJSON, object)
 
 -- | Build the index for a PackageInfo (except for the "indexed" value)
 buildIndexPairs :: String                       -- ^ Name of function / type / module / etc.
                 -> PackageInfo                  -- ^ FunctionInfo record
-                -> [(Text, A.Value)]            -- ^ Pairs comprising the index for this document
+                -> [APair]                      -- ^ Pairs comprising the index for this document
 buildIndexPairs pkgName pkgInfo = kvpairs
   where
     kvpairs =
@@ -50,60 +27,57 @@ buildIndexPairs pkgName pkgInfo = kvpairs
 buildDocument :: UTCTime         -- ^ The indexed time
               -> String          -- ^ The function / method / type name
               -> PackageInfo     -- ^ The FunctionInfo record
-              -> A.Value         -- ^ Document object (as JSON)
+              -> Value         -- ^ Document object (as JSON)
 buildDocument now pkgName pkgInfo =
-  A.object  $
+  object  $
   [ pair "description"
-         (A.object $ [ -- description, author, category, dependencies, indexed, maintainer, name, synopsis
-                       -- type, version
-                       pair "indexed"      nowD
-                     , pair "description"  (p_description pkgInfo)
-                     , pair "author"       (p_author pkgInfo)
-                     , pair "category"     (p_category pkgInfo)
-                     , pair "dependencies" (p_dependencies pkgInfo)
-                     , pair "maintainer"   (p_maintainer pkgInfo)
-                     , pair "name"         (p_name pkgInfo)
-                     , pair "synopsis"     (p_synopsis pkgInfo)
-                     , pair "version"      (p_version pkgInfo)
-                     ]
+         (object $ [ -- description, author, category, dependencies, indexed, maintainer, name, synopsis
+                     -- type, version
+                     pair "indexed"      nowD
+                   , pair "description"  (p_description pkgInfo)
+                   , pair "author"       (p_author pkgInfo)
+                   , pair "category"     (p_category pkgInfo)
+                   , pair "dependencies" (p_dependencies pkgInfo)
+                   , pair "maintainer"   (p_maintainer pkgInfo)
+                   , pair "name"         (p_name pkgInfo)
+                   , pair "synopsis"     (p_synopsis pkgInfo)
+                   , pair "version"      (p_version pkgInfo)
+                   ]
          )
   , pair "index"       index
   , pair "uri"         uri
   ]
   where
-    nowD     = A.toJSON $ fmtDateHTTP now       -- date formatted for the document
-    nowI     = A.toJSON $ fmtDateXmlSchema now  -- date formatted for the index
+    nowD     = toJSON $ fmtDateHTTP now       -- date formatted for the document
+    nowI     = toJSON $ fmtDateXmlSchema now  -- date formatted for the index
     uri      = "http://hackage.haskell.org/package/" ++ (p_name pkgInfo)
-    index    = A.object $ [ pair "indexed" nowI ] ++ buildIndexPairs pkgName pkgInfo
+    index    = object $ [ pair "indexed" nowI ] ++ buildIndexPairs pkgName pkgInfo
 
 -- | Build the Delete command for a list of packages.
-buildDeletes :: [String] -> A.Value
+buildDeletes :: [String] -> Value
 buildDeletes pkgNames =
-  A.object
+  object
   [ pair "cmd"   "delete-by-query"
-  , pair "query" (A.object [ pair "args" [arg1, arg2]
-                           , pair "op"   "and"
-                           , pair "type" "seq"
-                           ]
+  , pair "query" (object [ pair "args" [arg1, arg2]
+                         , pair "op"   "and"
+                         , pair "type" "seq"
+                         ]
                  )
   ]
-  where arg1 = A.object [ pair "type"    "context"
-                        , pair "contexts" [ "type" ]
-                        , pair "query"    (fullWord "package")
-                        ]
-        arg2 = A.object [ pair "type"     "context"
-                        , pair "contexts" [ "name" ]
-                        , pair "query"    (A.object [ pair "args" (map fullWord pkgNames)
-                                                   , pair "op"   "or"
-                                                   , pair "type" "seq"
-                                                   ]
-                                          )
-                        ] 
-        fullWord s = A.object [ pair "op"   "case"
-                              , pair "type" "fullword"
-                              , pair "word" s
-                              ]
-
-buildNOOP :: A.Value
-buildNOOP = A.object [ pair "cmd" "noop" ]
+  where arg1 = object [ pair "type"    "context"
+                      , pair "contexts" [ "type" ]
+                      , pair "query"    (fullWord "package")
+                      ]
+        arg2 = object [ pair "type"     "context"
+                      , pair "contexts" [ "name" ]
+                      , pair "query"    (object [ pair "args" (map fullWord pkgNames)
+                                                 , pair "op"   "or"
+                                                 , pair "type" "seq"
+                                                 ]
+                                        )
+                      ] 
+        fullWord s = object [ pair "op"   "case"
+                            , pair "type" "fullword"
+                            , pair "word" s
+                            ]
 

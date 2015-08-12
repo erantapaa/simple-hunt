bcd6e403b4295a0c2da0e98c09bfe691c2225794
Erik Rantapaa <erantapaa@gmail.com>
2015-08-12 08:31:48 -0500

    More refactoring of MainHoogle.hs

--- src/MainHoogle.hs
+++ src/MainHoogle.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE Rank2Types #-}
 
 module MainHoogle where
 
@@ -13,14 +14,39 @@ import           System.IO
 import           System.Directory
 import           System.FilePath
 import qualified Codec.Archive.Tar as Tar
+import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as LBS
 import           TarUtil (pipesTarEntries)
+import qualified Data.Text as Text
+
+import Control.Monad.Trans.State.Strict
  
 checkFileExists path = do
   ok <- doesFileExist path
-  if not ok
-    then error $ "file does not exist: " ++ path
-    else return ()
+  when (not ok) $ error $ "file does not exist: " ++ path
+
+type LinesProducer = MonadIO m => Producer (Int, Text.Text) m ()
+
+-- process a Hoogle file represetned as stream of strict ByteStrings.
+
+processHoogle :: (String -> Maybe Float)   -- ^ Score function
+              -> J.UTCTime                 -- ^ Index time
+              -> [ J.Value ]               -- ^ Commands to emit before inserts
+              -> LinesProducer             -- ^ Lines of the hoogle file
+              -> Handle                    -- ^ Output file handle
+              -> IO ()
+processHoogle scoreFn now deleteCmds linesStream fh = do
+  hPutStrLn fh "["
+  if null deleteCmds
+    then J.hJsonPutStr True fh J.buildNOOP
+    else forM_ deleteCmds $ J.hJsonPutStr True fh
+  evalHState $ linesStream
+                 >-> toHoogleLine
+                 >-> toFunctionInfo
+                 >-> toCommands scoreFn now
+                 >-> emitCommaJson fh
+  hPutStrLn fh "]"
+
 
 -- process a list of Hoogle files
 processHoogleFiles :: FilePath -> Bool -> [FilePath] -> IO ()
@@ -31,23 +57,10 @@ processHoogleFiles scorePath emitDeleteCmd paths = do
   now <- getCurrentTime
   forM_ paths $ \path -> do
     putStrLn $ "processing " ++ path
-    processHoogleFile scoreFn now emitDeleteCmd path
-
--- Process a single Hoogle file
-processHoogleFile scoreFn now emitDeleteCmd path = do
-  let pkgName = dropExtension $ takeBaseName path
-      jsonPath = "json/" ++ pkgName ++ ".js"
-  fh <- openFile jsonPath WriteMode
-  hPutStrLn fh "["
-  if emitDeleteCmd
-    then J.hJsonPutStr True fh (FC.buildDelete pkgName)
-    else J.hJsonPutStr True fh J.buildNOOP
-  evalHState $ skipHeader path
-                 >-> toHoogleLine
-                 >-> toFunctionInfo
-                 >-> toCommands scoreFn now
-                 >-> emitCommaJson fh
-  hPutStrLn fh "]"
+    let pkgName = dropExtension $ takeBaseName path
+        jsonPath = "json/" ++ pkgName ++ ".js"
+        deletes = if emitDeleteCmd then [ FC.buildDelete pkgName ] else []
+    withFile jsonPath WriteMode $ processHoogle scoreFn now deletes (skipHeader path)
 
 -- Process all Hoogle files in a tar archive.
 processHoogleTarArchive scorePath now emitDeleteCmd path = do
@@ -62,18 +75,10 @@ processHoogleTarArchive scorePath now emitDeleteCmd path = do
         Just (name, content) -> yield (name, content)
     go2 scoreFn now (pkgName, content) = lift $ do
       let jsonPath = "json/" ++ pkgName ++ ".js"
-      fh <- openFile jsonPath WriteMode
-      hPutStrLn fh "["
-      if emitDeleteCmd
-        then J.hJsonPutStr True fh (FC.buildDelete pkgName)
-        else J.hJsonPutStr True fh J.buildNOOP
-      evalHState $ skipHeaderLBS content
-                     >-> toHoogleLine
-                     >-> toFunctionInfo
-                     >-> toCommands scoreFn now
-                     >-> emitCommaJson fh
-      hPutStrLn fh "]"
+          deletes = if emitDeleteCmd then [ FC.buildDelete pkgName ] else []
+      withFile jsonPath WriteMode $ processHoogle scoreFn now deletes (skipHeaderLBS content)
 
+-- Select only normal files from a tar archive.
 parseTarEntry :: Tar.Entry -> Maybe (String, LBS.ByteString)
 parseTarEntry ent = do
   (content, len) <- getNormalFileContent $ Tar.entryContent ent
@@ -83,20 +88,4 @@ parseTarEntry ent = do
   where
     getNormalFileContent (Tar.NormalFile content len) = Just (content,len)
     getNormalFileContent _                            = Nothing
-
-{-
-processHoogleContent scoreFn now emitDeleteCmd pkgName content = do
-  let jsonPath = "json/" ++ pkgName ++ ".js"
-  fh <- openFile jsonPath WriteMode
-  hPutStrLn fh "["
-  if emitDeleteCmd
-    then J.hJsonPutStr True fh (FC.buildDelete pkgName)
-    else J.hJsonPutStr True fh J.buildNOOP
-  evalHState $ skipHeader path
-                 >-> toHoogleLine
-                 >-> toFunctionInfo
-                 >-> toCommands scoreFn now
-                 >-> emitCommaJson fh
-  hPutStrLn fh "]"
--}
 
